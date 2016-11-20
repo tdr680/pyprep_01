67d55010181cc95e5824a6226c0ee6d67d52143c
tdr680 <tdr680@yahoo.de>
2016-11-20 15:11:14 +0100

    First seven exercises

---
+++ ex_a.py
@@ -0,0 +1,40 @@
+
+
+# TODO 1) create variable that contains list of even numbers bigger than 0 and smaller than 20 (using range function)
+
+
+
+# TODO 2) given variables a and b print (using format) sentence "Karel is 39 years old."
+
+a = "Karel"
+b = 39
+
+
+
+# TODO 3) print odd numbers bigger than 0 and smaller than 20 using while loop (and modulo operator)
+
+
+
+# TODO 4) open file "primes.txt" and print the prime numbers in the file that are between 100 and 200
+
+
+
+# TODO 5) complete function below to create file "months.txt" that stores the names of the months (using for loop)
+
+months = ['January', 'February', 'March', 'April', 'Mai', 'June',
+          'July', 'August', 'September', 'October', 'November', 'December']
+
+def write_months(file_name):
+    # your code here
+    # ...
+
+write_months('months.txt')
+
+
+# TODO 6) write a function that accepts arbitrary number of numbers and returns their average
+
+
+
+# TODO 7) write a function having three arguments: first_name, last_name, age and returns dictionary:
+#         {'first_name': first argument, 'last_name': second argument, 'age': third argument}
+
