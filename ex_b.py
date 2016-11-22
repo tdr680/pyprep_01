2725fc7e06545e2da29e11afec5aa48bb7fc41f9
tdr680 <tdr680@yahoo.de>
2016-11-22 23:01:35 +0100

    Second batch of exercises

---
+++ ex_b.py
@@ -0,0 +1,35 @@
+
+# TODO 1) write a function celsius_to_fahrenheit which returns grades of fahrenheit given grades of celsius
+#         For example celsius_to_fahrenheit(37) returns 98.6
+
+# HINT: http://www.almanac.com/content/temperature-conversion
+
+
+# TODO 2) write a function fahrenheit_to_celsius which returns grades of celsius given grades of fahrenheit
+#         For example fahrenheit_to_celsius(98.6) returns 37.0
+
+
+# TODO 3) write a function quad_roots which returns tuple with zero, one or two roots of quadratic equation
+#         having three parameters a, b and c which are coefficients of a*x*x + b*x + c = 0
+#         For example quad_roots(1, 2, 1) should return (-1)
+
+# HINT: http://www.bbc.co.uk/schools/gcsebitesize/maths/algebra/quadequationshirev3.shtml
+
+
+# TODO 4) write a function float_range(start, end, step) which returns list of floats from start to end
+#         jumping by step, where all three parameters may be float numbers.
+#         For example float_range(4, 6, 0.5) should return [4.0, 4.5, 5.0, 5.5, 6.0]
+
+# HINT: create empty array e.g. result, loop using while and append to the result
+
+
+# TODO 5) write a function longest_string having variable number of string parameters, which returns
+#         the longest parameter. For example longest_string("aaa", "a", "aaaa", "aa") returns "aaaa"
+
+# HINT: https://pythontips.com/2013/08/04/args-and-kwargs-in-python-explained/
+
+
+# TODO 6) write a function char_freq having one string parameter, which returns frequency of characters in string
+#         For example char_freq("aabrracadaabra") returns dictionary: {'a': 7, 'b': 2, 'r': 3, 'c': 1, 'd': 1}
+
+# HINT: create empty dictionary e.g. result, loop over string using for loop and add every character to the result
