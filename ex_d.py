7b2d6524c9f5f2708e7197860dc81461c4087114
tdr680 <tdr680@yahoo.de>
2016-12-02 15:41:47 +0100

    ex_d.py added

---
+++ ex_d.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+###################################################################################################
+
+def deck(suits, ranks):
+    # TODO: go through all suits and all ranks and print the deck, each combination on one line:
+    # A♧
+    # 1♧
+    # 2♧
+    # ...
+    pass # <- this to be replaced by code
+
+# now create two lists and call the function
+SUITS = ["♧", "♢", "♡", "♤"]
+RANKS = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+
+deck(SUITS, RANKS)
+
+###################################################################################################
