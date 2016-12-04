eb560089dff549f716d3e017135fff731a42c69c
tdr680 <tdr680@yahoo.de>
2016-12-04 14:21:06 +0100

    ACCOUNT: changing attribute balance to __balance. such attribute (starting by __) is accessible just by own methods and not directly, which prevents changing the value ot the attribute from outside. just deposit and withdraw methods can modify the balance.

--- account.py
+++ account.py
@@ -2,14 +2,16 @@
 class Account(object):
 
     def __init__(self):
-        self.balance = 0
+        self.__balance = 0
 
     def deposit(self, amount):
-        self.balance += amount
+        self.__balance += amount
 
     def withdraw(self, amount):
-        self.balance -= amount
+        self.__balance -= amount
 
+    def balance(self):
+        return self.__balance
 
 if __name__ == "__main__":
 
@@ -17,9 +19,9 @@ if __name__ == "__main__":
     acc_a.deposit(250)
     acc_a.deposit(10)
     acc_a.withdraw(100)
-    print acc_a.balance # 160
+    print acc_a.balance() # 160
 
     acc_b = Account()
     acc_b.withdraw(200)
-    print acc_b.balance # -200
+    print acc_b.balance() # -200
 
