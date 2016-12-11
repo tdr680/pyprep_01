d9d974521ad2076bfc848d928b78fde2f451368f
tdr680 <tdr680@yahoo.de>
2016-12-11 16:39:00 +0100

    First two tasks
    Signed-off-by: tdr680 <tdr680@yahoo.de>

---
+++ ex_e.py
@@ -0,0 +1,12 @@
+
+# TODO 1 Calculating the Factors of an Integer
+#        When a nonzero integer, a, divides another integer, b, leaving a remainder 0, a is said to be a factor of b
+#        As an example, 2 is a factor of all even integers hence e.g. is_factor(2, 8) should return True
+
+
+# TODO 2 Using is_factor function write another function print_factors(n) which prints for n = 20
+#      2 is a factor of 20
+#      4 is a factor of 20
+#      5 is a factor of 20
+#      10 is a factor of 20
+
