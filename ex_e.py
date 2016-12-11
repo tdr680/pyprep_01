2579336b4ae09a34bea4f791e67d7d39b0c106ad
tdr680 <tdr680@yahoo.de>
2016-12-11 22:16:36 +0100

    5-10 added

--- ex_e.py
+++ ex_e.py
@@ -21,3 +21,56 @@
 # TODO 4 Using previous function prints the leap years years between 2000 and 2050
 
 
+# TODO 5 Following dictionary uses as a key upper case character and as the value morse code.
+#        Write function to_morse(str) having as a parameter arbitrary string containing characters and returns
+#        morse code of the argument. E.g. to_morse("Hello world") returns: ".... . .-.. .-.. --- / .-- --- .-. .-.. -.."
+
+MORSE = {'A': '.-',     'B': '-...',   'C': '-.-.',
+         'D': '-..',    'E': '.',      'F': '..-.',
+         'G': '--.',    'H': '....',   'I': '..',
+         'J': '.---',   'K': '-.-',    'L': '.-..',
+         'M': '--',     'N': '-.',     'O': '---',
+         'P': '.--.',   'Q': '--.-',   'R': '.-.',
+         'S': '...',    'T': '-',      'U': '..-',
+         'V': '...-',   'W': '.--',    'X': '-..-',
+         'Y': '-.--',   'Z': '--..',   ' ': '/'
+         }
+
+
+# TODO 6 Extend MORSE dictionary to include numbers and try to_morse("Anno 2070")
+#   1    .----
+#   2    ..---
+#   3    ...--
+#   4    ....-
+#   5    .....
+#   6    -....
+#   7    --...
+#   8    ---..
+#   9    ----.
+#   0    -----
+
+
+# TODO 7 Write class Character having three attributes: name, race, faction and create objects:
+#        player_1 = Character("Gringo", "Dwarf", "Alliance")
+#        player_2 = Character("Balath", "Orc", "Horde")
+
+
+# TODO 8 Implement __str__ for Character class that print player_1 outputs:
+#        Player  Gringo
+#        Race    Dwarf
+#        Faction Alliance
+
+
+# TODO 9 Modify constructor to initiate one more attribute: talent - dictionary that describes charater's staticstic.
+#        player_1 = Character("Gringo", "Dwarf", "Alliance",
+#                            {"strength" : 3, "dexterity" : 8, "intelligence" : 10, "health" : 10})
+#        player_2 = Character("Balath", "Orc", "Horde",
+#                            {"strength": 10, "dexterity": 5, "intelligence": 3, "health": 10})
+
+
+# TODO 10 Improve __str__ for Character class that print player_1 outputs its statistic too:
+#        Player  Gringo
+#        Race    Dwarf
+#        Faction Alliance
+#        S/D/I/H 3/8/10/10
+
